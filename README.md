19376303b8f9bee78e82b6af2562b8b992e8db39
James Hodgkinson <james.hodgkinson@webcentralgroup.com.au>
2020-08-30 15:12:33 +1000

    updated to use config file

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # jsd_job_bot
 Shows which jobs are in my JSD queue
 
-You need a `config.py` file:
+You need a `jsd_job_bot.ini` config file:
 
-    JSD_API_KEY = 'supersecretAPIkey'
-    JSD_HOSTNAME = 'example.atlassian.net'
-    JSD_USERNAME = 'user@example.com'
-    SHOW_ALL_JOBS = False
+    [DEFAULT]
+    JSD_API_KEY=<your api key>
+    JSD_HOSTNAME=<your instance>.atlassian.net
+    JSD_USERNAME=<your email address>
+
+You can also set `SHOW_ALL_JOBS` which will show everything regardless of status.
+
+The config file needs to be in one of these places:
+
+* The current working directory.
+* `~/.config`
+* `~/etc/`
 
 Run it, it'll show you what you've got assigned to you.
 
--- jsd_job_bot.py
+++ jsd_job_bot.py
@@ -3,20 +3,31 @@
 """ polls JSD to find tickets I'm assigned to """
 
 import os
-import dateutil.parser
+from configparser import ConfigParser
 
+import dateutil.parser
 from jira import JIRA
+import jira.exceptions
 from texttable import Texttable
-from config import JSD_API_KEY, JSD_HOSTNAME, JSD_USERNAME, SHOW_ALL_JOBS
+
 
 if not os.environ.get('LOGURU_LEVEL'):
     os.environ['LOGURU_LEVEL'] = "INFO"
 from loguru import logger #pylint: disable=wrong-import-position,wrong-import-order
 
-jira_client_object = JIRA(
-    f"https://{JSD_HOSTNAME}",
-    basic_auth=(JSD_USERNAME, JSD_API_KEY),
-    )
+config = ConfigParser()
+for config_file in ['~/.config/jsd_job_bot.ini',
+                    '~/etc/jsd_job_bot.ini',
+                    './jsd_job_bot.ini',
+                    ]:
+    if os.path.exists(config_file):
+        logger.debug(f"Loading config: {config_file}")
+        config.read(config_file)
+
+# don't show things by default
+if not config.has_option('DEFAULT', 'SHOW_ALL_JOBS'):
+    logger.debug("Setting show all jobs to false, currently unset")
+    config.set('DEFAULT', 'SHOW_ALL_JOBS', 'false')
 
 JSD_SEARCH = """assignee = currentuser() ORDER BY updated ASC"""
 
@@ -24,11 +35,9 @@ IGNORED_STATUS = [
     'Completed',
     'Done',
     'Canceled',
-    'Resolved'
+    'Resolved',
+    'Closed',
 ]
-
-
-issues = jira_client_object.search_issues(jql_str=JSD_SEARCH, maxResults=False) # noqa: pylint: disable=invalid-name
 
 def ignore_this_field(fieldname):
     """ filters fields """
@@ -42,15 +51,32 @@ ASSIGNED_ISSUES = 0
 table = Texttable()
 table.set_cols_dtype(['a', 'a', 'a', 'a'])
 table.set_max_width(0)
-
 table.set_deco(Texttable.BORDER + Texttable.VLINES + Texttable.HEADER)
-
 table.add_row(['Issue', 'Status', 'Updated', 'Summary'])
+try:
+    logger.debug(f"JIRA Server: '{config.get('DEFAULT', 'JSD_HOSTNAME')}'")
+    logger.debug(f"JIRA Username: '{config.get('DEFAULT', 'JSD_USERNAME')}'")
+    logger.debug(f"JIRA API Key: '{config.get('DEFAULT', 'JSD_API_KEY')}'")
+    logger.debug("Instantiating JIRA connection...")
+    jira_client_object = JIRA(
+        f"https://{config.get('DEFAULT', 'JSD_HOSTNAME')}",
+        basic_auth=(config.get('DEFAULT', 'JSD_USERNAME'), config.get('DEFAULT', 'JSD_API_KEY')),
+        )
+    logger.debug("Done instantiating JIRA connection...")
+except ConnectionError as error_message:
+    logger.error(f"Connection error: {error_message}")
 
-
+try:
+    logger.debug("Searching for jobs assigned to you...")
+    issues = jira_client_object.search_issues(jql_str=JSD_SEARCH, maxResults=False)
+    # noqa: pylint: disable=invalid-name
+except jira.exceptions.JIRAError as error_message:
+    logger.error(error_message)
+    exit()
 
 for issue in issues:
-    if SHOW_ALL_JOBS or str(issue.fields.status) not in IGNORED_STATUS:
+    logger.debug(f"issue status: '{issue.fields.status}'")
+    if config.getboolean('DEFAULT', 'SHOW_ALL_JOBS') or str(issue.fields.status).strip() not in IGNORED_STATUS:
         ASSIGNED_ISSUES += 1
         updated_parsed = dateutil.parser.parse(issue.fields.updated)
 
@@ -60,7 +86,7 @@ for issue in issues:
                        updated_parsed.date(),
                        str(issue.fields.summary),
                        ])
-        logger.debug([dirfield for dirfield in dir(issue.fields) if not ignore_this_field(dirfield)])
+        #logger.debug([dirfield for dirfield in dir(issue.fields) if not ignore_this_field(dirfield)])
 print(table.draw())
 
 logger.info(f"{ASSIGNED_ISSUES} tickets assigned to you...")
